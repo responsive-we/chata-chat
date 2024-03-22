ee70aadf0ac97395294e394020e206a29122ddb6
responsive-we <98676194+responsive-we@users.noreply.github.com>
2024-03-23 01:34:34 +0530

    Fixing CSS of Auth component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../styles/auth.css";
 import Add from "../assets/addAvatar.png";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
 import google from "../assets/google.png";
 import {
   auth,
@@ -21,7 +22,7 @@ import {
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 const ErrorDisplay = ({ err }) => (
   <div className="error__display">
     <span>{err}</span>
@@ -178,45 +179,126 @@ const Auth = () => {
     },
   });
 
- 
+const SignInForm=()=>(
+  <form onSubmit={formik.handleSubmit}>
+  <div className="form sign-in">
+    <h2>Welcome</h2>
+    <label>
+      <span>Email</span>
+      <input
+        onChange={formik.handleChange}
+        type="email"
+        name="email"
+        value={formik.values.email}
+      />
+      {formik.errors.email && <ErrorDisplay err={formik.errors.email} />}
+    </label>
+    <label className="mb-3">
+      <span>Password</span>
+      <input
+        onChange={formik.handleChange}
+        type="password"
+        name="password"
+      />
+    </label>
+    {err && <ErrorDisplay err="Invalid credentials" />}
+    <hr/>
+    <div className="flex justify-center items-center flex-col">
+    <button type="submit" className=" mt-4 h-9 w-28 bg-transparent text-white cursor-pointer border-solid border-white border-2 "> 
+      Sign In
+    </button>
+    <div className="continue__with__wrapper">
+      <button type="button" className="continue__with" onClick={continueWithGoogle}>
+        <img src={google} alt="" />
+        <span>Continue with Google</span>
+      </button>
+    </div>
+    </div>
+  </div>
+</form>
+)
 
+const SignUpForm=()=>(
+  <form onSubmit={formik.handleSubmit}>
+  <div className="form sign-up">
+    <h2>Create your Account</h2>
+    <label>
+      <span>Name</span>
+      <input
+        onChange={formik.handleChange}
+        type="text"
+        name="name"
+        value={formik.values.name}
+      />
+      {formik.errors.name && <ErrorDisplay err={formik.errors.name} />}
+    </label>
+    <label>
+      <span>Email</span>
+      <input
+        onChange={formik.handleChange}
+        type="email"
+        name="email"
+        value={formik.values.email}
+      />
+      {formik.errors.email && (
+        <ErrorDisplay err={formik.errors.email} />
+      )}
+    </label>
+    <label>
+      <span>Password</span>
+      <input
+        onChange={formik.handleChange}
+        type="password"
+        name="password"
+        value={formik.values.password}
+      />
+      {formik.errors.password && (
+        <ErrorDisplay err={formik.errors.password} />
+      )}
+    </label>
+    <input
+      style={{ display: "none" }}
+      accept="image/*"
+      name="avatar"
+      onChange={handleAvatar}
+      type="file"
+      id="file"
+    />
+    <label htmlFor="file" className="labelForFile mb-3">
+      <Avatar>
+        {avatarUrl &&
+        <AvatarImage src={avatarUrl}/>}
+        <PlusCircledIcon height={30} width={30}/>
+      </Avatar>
+      {/* <img src={avatarUrl ? avatarUrl : Add} alt="Avatar" className="" /> */}
+      <span className="ml-2">{avatar ? "Looking good !" : "Add an avatar"}</span>
+    </label>
+    {err && (
+      <ErrorDisplay err="Something went wrong, Could not Sign up. Please try again later" />
+    )}
+
+    <hr />
+    <div className="flex justify-center items-center flex-col">
+    <button type="submit" className="mt-4 h-9 w-28 bg-transparent text-white cursor-pointer border-solid border-white border-2 ">
+      Sign Up
+    </button>
+    <div className="continue__with__wrapper">
+      <button
+        className="continue__with"
+        type="button"
+        onClick={continueWithGoogle}
+      >
+        <img src={google} alt="" />
+        <span>Continue with Google</span>
+      </button>
+    </div>
+    </div>
+  </div>
+</form>
+  )
   return (
-    <div className={styleClass}>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="form sign-in">
-          <h2>Welcome</h2>
-          <label>
-            <span>Email</span>
-            <input
-              onChange={formik.handleChange}
-              type="email"
-              name="email"
-              value={formik.values.email}
-            />
-            {formik.errors.email && <ErrorDisplay err={formik.errors.email} />}
-          </label>
-          <label>
-            <span>Password</span>
-            <input
-              onChange={formik.handleChange}
-              type="password"
-              name="password"
-            />
-          </label>
-          {err && <ErrorDisplay err="Invalid credentials" />}
-
-          <button type="submit" className="submit">
-            Sign In
-          </button>
-          <hr />
-          <div className="continue__with__wrapper">
-            <button className="continue__with" onClick={continueWithGoogle}>
-              <img src={google} alt="" />
-              <span>Continue with Google</span>
-            </button>
-          </div>
-        </div>
-      </form>
+    <div className={`bg-slate-700 ${styleClass}`}>
+  {isSignUp?<SignUpForm/>:<SignInForm/>}
       <div className="sub-cont">
         <div className="img">
           <div className="img__text m--up">
@@ -230,74 +312,6 @@ const Auth = () => {
             <span className="m--in">Sign In</span>
           </div>
         </div>
-        <form onSubmit={formik.handleSubmit}>
-          <div className="form sign-up">
-            <h2>Create your Account</h2>
-            <label>
-              <span>Name</span>
-              <input
-                onChange={formik.handleChange}
-                type="text"
-                name="name"
-                value={formik.values.name}
-              />
-              {formik.errors.name && <ErrorDisplay err={formik.errors.name} />}
-            </label>
-            <label>
-              <span>Email</span>
-              <input
-                onChange={formik.handleChange}
-                type="email"
-                name="email"
-                value={formik.values.email}
-              />
-              {formik.errors.email && (
-                <ErrorDisplay err={formik.errors.email} />
-              )}
-            </label>
-            <label>
-              <span>Password</span>
-              <input
-                onChange={formik.handleChange}
-                type="password"
-                name="password"
-                value={formik.values.password}
-              />
-              {formik.errors.password && (
-                <ErrorDisplay err={formik.errors.password} />
-              )}
-            </label>
-            <input
-              style={{ display: "none" }}
-              accept="image/*"
-              name="avatar"
-              onChange={handleAvatar}
-              type="file"
-              id="file"
-            />
-            <label htmlFor="file" className="labelForFile">
-              <img src={avatarUrl ? avatarUrl : Add} alt="Avatar" />
-              <span>{avatar ? "Looking good" : "Add an avatar"}</span>
-            </label>
-            {err && (
-              <ErrorDisplay err="Something went wrong, Could not Sign up. Please try again later" />
-            )}
-            <button type="submit" className="submit">
-              Sign Up
-            </button>
-            <hr />
-            <div className="continue__with__wrapper">
-              <button
-                className="continue__with"
-                type="button"
-                onClick={continueWithGoogle}
-              >
-                <img src={google} alt="" />
-                <span>Continue with Google</span>
-              </button>
-            </div>
-          </div>
-        </form>
       </div>
     </div>
   );
